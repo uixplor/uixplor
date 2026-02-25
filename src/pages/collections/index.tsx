4d28e2a735a19ca97b54505293583f56eff5758b
Sarabjeet Singh <ss4039417@gmail.com>
2026-02-25 23:08:37 +0530

    main

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -103,7 +103,7 @@ export default function Collections() {
 
 					{/* Collections Grid */}
 					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-6">
-						{collections.map((collection, index) => (
+						{collections.filter(c => c.id !== 'ui-tokens').map((collection, index) => (
 							<motion.div
 								key={collection.id}
 								initial={{ opacity: 0, y: 20 }}
